39fad1e17dff3bcdd288e0c14029b79144c31a3e
Stefan Touhami <Stefan@Touhami.pl>
2021-07-25 11:34:25 +0200

    Add ECS as coding style guard

---
+++ ecs.php
@@ -0,0 +1,21 @@
+<?php
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+    ]);
+
+    $containerConfigurator->import(SetList::SPACES);
+    $containerConfigurator->import(SetList::ARRAY);
+    $containerConfigurator->import(SetList::DOCBLOCK);
+    $containerConfigurator->import(SetList::PSR_12);
+    $containerConfigurator->import(SetList::NAMESPACES);
+    $containerConfigurator->import(SetList::CLEAN_CODE);
+    $containerConfigurator->import(SetList::SYMFONY);
+};
